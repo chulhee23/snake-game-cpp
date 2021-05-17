8077cacc4a1355ed7874a160997c90df7ec03397
chulhee23 <chulhee2351@gmail.com>
2021-05-18 02:09:49 +0900

    gitignore

--- snakeMap.cpp
+++ snakeMap.cpp
@@ -1,9 +1,13 @@
 #include <ncurses.h>
 
+const int WINDOW_W = 30;
+const int WINDOW_H = 100;
+
+
 int main(){
-  WINDOW* win1; // snake map
-  WINDOW* win2; // score board
-  WINDOW* win3; // username board
+  WINDOW* snake_map; // snake map
+  WINDOW* score_board; // score board
+  WINDOW* user_board; // username board
 
   int testArr[5][8] = {
     {2, 1, 1, 1, 1, 1, 1, 2},
@@ -29,48 +33,49 @@ int main(){
   mvprintw(1, 1, "SNAKE GAME");
   attroff(COLOR_PAIR(2));
 
-  win1 = newwin(23, 23, 3, 3);
-  wattron(win1, COLOR_PAIR(1));
-  wborder(win1, '|', '|', '-', '-', '+', '+', '+', '+');
-  mvwprintw(win1, 0, 8, "SNAKE");
-  wattroff(win1, COLOR_PAIR(1));
+  snake_map = newwin(23, 23, 3, 3);
+  wattron(snake_map, COLOR_PAIR(1));
+  wborder(snake_map, '|', '|', '-', '-', '+', '+', '+', '+');
+  mvwprintw(snake_map, 0, 8, "SNAKE");
+  wattroff(snake_map, COLOR_PAIR(1));
   
   for(int i = 0; i < 5; i++){
     for(int j = 0; j < 8; j++){
       switch(testArr[i][j]){
         case 1:
-          wattron(win1, COLOR_PAIR(5));
-          mvwprintw(win1, i+1, j+1, " ");
-          wattroff(win1, COLOR_PAIR(5));
+          wattron(snake_map, COLOR_PAIR(5));
+          mvwprintw(snake_map, i+1, j+1, " ");
+          wattroff(snake_map, COLOR_PAIR(5));
           break;
         case 2:
-          wattron(win1, COLOR_PAIR(2));
-          mvwprintw(win1, i+1, j+1, " ");
-          wattroff(win1, COLOR_PAIR(2));
+          wattron(snake_map, COLOR_PAIR(2));
+          mvwprintw(snake_map, i+1, j+1, " ");
+          wattroff(snake_map, COLOR_PAIR(2));
           break;
         case 3:
-          wattron(win1, COLOR_PAIR(3));
-          mvwprintw(win1, i+1, j+1, " ");
-          wattroff(win1, COLOR_PAIR(3));
+          wattron(snake_map, COLOR_PAIR(3));
+          mvwprintw(snake_map, i+1, j+1, " ");
+          wattroff(snake_map, COLOR_PAIR(3));
           break;
         case 4:
-          wattron(win1, COLOR_PAIR(4));
-          mvwprintw(win1, i+1, j+1, " ");
-          wattroff(win1, COLOR_PAIR(4));
+          wattron(snake_map, COLOR_PAIR(4));
+          mvwprintw(snake_map, i+1, j+1, " ");
+          wattroff(snake_map, COLOR_PAIR(4));
           break;
       }
     }
   }
 
-  win2 = newwin(4, 10, 5, 38);
-  wattron(win2, COLOR_PAIR(1));
-  mvwprintw(win2, 1, 1, "SCORE : ");
-  mvwprintw(win2, 2, 1, "0");
-  wborder(win2, '|', '|', '-', '-', '+', '+', '+', '+');
+  score_board = newwin(4, 10, 5, 38);
+  
+  wattron(score_board, COLOR_PAIR(1));
+  mvwprintw(score_board, 1, 1, "SCORE : ");
+  mvwprintw(score_board, 2, 1, "0");
+  wborder(score_board, '|', '|', '-', '-', '+', '+', '+', '+');
 
   refresh();
-  wrefresh(win1);
-  wrefresh(win2);
+  wrefresh(snake_map);
+  wrefresh(score_board);
 
   getch();
   endwin();
